d515fbb3133c50a492fc7f8552abbb148ae0924d
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2022-07-31 18:58:38 +0600

    Product derail design update

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -5,28 +5,45 @@ import axios from 'axios'
 import { useEffect } from 'react'
 
 const ProductDetail = () => {
-    const {id}=useParams()
-    const [product,setProduct]=useState([])
+    const { id } = useParams()
+    const [product, setProduct] = useState([])
 
     const getProductDetail = (id) => {
         axios.get(`${domain}/product/${id}/`)
             .then((response) => {
                 const product = response.data
                 setProduct(product)
-                // console.log("product",product)
             })
             .catch(error => console.error(`Error : ${error}`))
     }
     useEffect(() => {
         getProductDetail(id)
     }, [])
-  return (
-    <div>
-        <h1>This Is a Product Detail Page For</h1>
-        <h1>{product.title}</h1>
-        <img src={product.image} alt="" height="250px" width="250px" />
-    </div>
-  )
+    return (
+        <div className="my-3">
+            <h1 className="text-center text-danger fw-bold">Product Detail</h1>
+            <div className="container my-3">
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-6 my-2">
+                        <img src={product.image} alt="" height="300px" width="300px"/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-6 my-2">
+                        <h3 className='my-2 text-success text-uppercase'>Product Name :  {product.title}</h3>
+                        <h3 className='my-2 text-success text-uppercase'>Market Price :  {product.market_price}</h3>
+                        <h3 className='my-2 text-success text-uppercase'>Selling Price :  {product.selling_price}</h3>
+
+                        <h3 className='my-2 text-success text-uppercase'>Description :</h3>
+                        <p>{product.description}</p>
+
+                        <div className='text-center'>
+                            <button className='btn btn-outline-warning w-50'>Add to Cart</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ProductDetail
