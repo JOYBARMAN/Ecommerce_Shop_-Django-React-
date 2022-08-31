875f4d4d06a041651e0c6e334865ee245fb200ec
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2022-08-31 10:52:14 +0600

    Category update

--- shop/views.py
+++ shop/views.py
@@ -14,12 +14,19 @@ class DetailProduct (generics.RetrieveAPIView):
     # lookup_field="id"
 
 class CategoryView (viewsets.ModelViewSet):
-    # def list (self,request):
-    #     query=Category.objects.all().order_by("-id")
-    #     serializers =CategorySerializers(query,many=True)
-    #     return Response(serializers.data)
-    queryset = Category.objects.all().order_by("-id")
-    serializer_class =CategorySerializers
+    def list (self,request):
+        query=Category.objects.all().order_by("-id")
+        serializers =CategorySerializers(query,many=True)
+        return Response(serializers.data)
+    # queryset = Category.objects.all().order_by("-id")
+    # serializer_class =CategorySerializers
+
+    def retrieve(self, request, pk=None):
+        query=Category.objects.get(id=pk)
+        serializers = CategorySerializers(query)
+        productCategory=Product.objects.filter(category_id=serializers.data['id'])
+        categorySerializers=ProductSerializers(productCategory,many=True)
+        return Response(categorySerializers.data)
 
 
 class ProductViewSet (viewsets.ModelViewSet):
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -3,38 +3,50 @@ import { useParams } from 'react-router-dom'
 import { domain } from '../env'
 import axios from 'axios'
 import { useEffect } from 'react'
+import SingleProduct from './SingleProduct'
 
 const ProductDetail = () => {
     const { id } = useParams()
     const [product, setProduct] = useState([])
+    const [recomProduct, setRecomProduct] = useState(null)
 
     const getProductDetail = (id) => {
         axios.get(`${domain}/product/${id}/`)
             .then((response) => {
                 const product = response.data
                 setProduct(product)
+                categoryProduct(product?.category)
             })
             .catch(error => console.error(`Error : ${error}`))
     }
     useEffect(() => {
         getProductDetail(id)
-    }, [])
+    },[])
+
+    const categoryProduct = (id) => {
+        axios.get(`${domain}/category/${id}/`)
+            .then((response) => {
+                const categoryProduct = response.data
+                setRecomProduct(categoryProduct)
+            })
+            .catch(error => console.error(`Error : ${error}`))
+    }
     return (
         <div className="my-3">
             <h1 className="text-center text-danger fw-bold">Product Detail</h1>
             <div className="container my-3">
                 <div className="row">
                     <div className="col-12 col-md-6 col-lg-6 my-2">
-                        <img src={product.image} alt="" height="300px" width="300px"/>
+                        <img src={product.image} alt="" height="300px" width="300px" />
                         <div className="container d-flex my-5">
                             <div className=''>
-                            <img src={product.image} alt="" height="70px" width="80px"/>
+                                <img src={product.image} alt="" height="70px" width="80px" />
                             </div>
                             <div className='mx-2'>
-                            <img src={product.image} alt="" height="70px" width="80px"/>
+                                <img src={product.image} alt="" height="70px" width="80px" />
                             </div>
                             <div className='ms-2'>
-                            <img src={product.image} alt="" height="70px" width="80px"/>
+                                <img src={product.image} alt="" height="70px" width="80px" />
                             </div>
                         </div>
                     </div>
@@ -51,6 +63,17 @@ const ProductDetail = () => {
                         </div>
 
                     </div>
+                </div>
+            </div>
+            <div className="container">
+                <h1 className='text-danger fw-bold'>Recommended Product</h1>
+                <div className="row">
+                    {recomProduct?.map((item, i) => (
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-3" key={i}>
+                            <SingleProduct item={item} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </div>
